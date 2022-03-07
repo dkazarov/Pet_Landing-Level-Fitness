392804fdca37af128c8e51c11e014e793ac52950
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-07 18:25:11 +0200

    header: swyper: add swiper slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+
+import Swiper, { Navigation, Pagination, Keyboard, Autoplay} from "swiper";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination, Keyboard, Autoplay],
+
+  autoplay: {
+    delay: 5000,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  speed: 400,
+  spaceBetween: 100,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
