d05d402513e96726faed2cbb28b5ba1366bcc407
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-31 02:43:07 +0300

    add efects to benefits

--- src/js/inputMask.js
+++ src/js/inputMask.js
@@ -1,12 +1,12 @@
 import IMask from 'imask';
 //Input Mask
 
-// const elementMain = document.querySelector('.registration__phone');
+const elementMain = document.querySelector('.registration__phone-input');
 const element = document.querySelector('.modal__form-phone');
 
 const maskOptions = {
-  mask: '(063) 625 55 44',
+  mask: '(000) 000 00 00',
 };
 
 const mask = IMask(element, maskOptions);
-// const maskMain = IMask(elementMain, maskOptions);
+const maskMain = IMask(elementMain, maskOptions);
