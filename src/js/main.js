0e32dad45a50c512212c8379ca924ea31d1e389c
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-30 22:34:32 +0300

    finish modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,66 @@
 import Swiper, { Navigation, Pagination, Keyboard, Autoplay } from 'swiper';
 
-document.addEventListener(
-  'DOMContentLoaded',
-  function () {
-    'use strict';
+('use strict');
 
-    const swiper = new Swiper('.swiper', {
-      modules: [Navigation, Pagination, Keyboard, Autoplay],
+// Swiper
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination, Keyboard, Autoplay],
 
-      autoplay: {
-        delay: 5000,
-      },
-
-      keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-      },
-
-      speed: 400,
-      spaceBetween: 100,
-
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-      },
-    });
-    // Ankor navigation
-    const anchors = document.querySelectorAll('a[href*="#"]');
-
-    for (let anchor of anchors) {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const blockID = anchor.getAttribute('href').substr(1);
-
-        document.getElementById(blockID).scrollIntoView({
-          behavior: 'smooth',
-          block: 'start',
-        });
-      });
-    }
+  autoplay: {
+    delay: 5000,
   },
-  false
-);
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  speed: 400,
+  spaceBetween: 100,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
+
+// Ankor navigation
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
+
+// Modal
+
+const headerBtn = document.querySelector('.header__action-top-btn');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+
+const openModal = () => {
+  overlay.classList.add('active');
+  modal.classList.add('fadeIn');
+};
+
+const closeModal = () => {
+  overlay.classList.remove('active');
+};
+
+headerBtn.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
