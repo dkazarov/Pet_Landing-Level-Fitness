76bd60cf3230ba4a181cda43fc6fb3cd7f6334f0
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-31 03:21:29 +0300

    add timer to modal & fix some bugs related

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,35 +6,55 @@ const close = document.querySelector('.modal__close');
 const modal = document.querySelector('.modal');
 const btnLarge = document.querySelector('.btn--large');
 
+// Open modal
 const openModal = () => {
   overlay.classList.add('active');
   modal.classList.add('fadeIn');
   document.body.style.overflow = 'hidden';
+  clearInterval(modalTimer);
 };
 
+// Close Modal
 const closeModal = () => {
   overlay.classList.remove('active');
   document.body.style.overflow = '';
 };
 
+// close modal
+const canseledDefaultBehavior = (e) => {
+  e.preventDefault;
+  closeModal();
+};
+
+// Listener on key ESCAPE
 document.addEventListener('keydown', (e) => {
   if (e.code === 'Escape' && overlay.classList.contains('active')) {
     closeModal();
   }
 });
 
-const canseledDefaultBehavior = (e) => {
-  e.preventDefault;
-  closeModal();
-};
-
+//examination click
 overlay.addEventListener('click', (e) => {
   if (e.target === overlay) {
     closeModal();
   }
 });
 
+// Show modal on Scroll
+const showModalByScroll = () => {
+  if (
+    window.pageYOffset + document.documentElement.clientHeight >=
+    document.documentElement.scrollHeight - 1
+  ) {
+    openModal();
+    window.removeEventListener('scroll', showModalByScroll);
+  }
+};
+
+// Open modal by time
+const modalTimer = setTimeout(openModal, 30000);
+
+window.addEventListener('scroll', showModalByScroll);
 headerBtn.addEventListener('click', openModal);
 btnLarge.addEventListener('click', canseledDefaultBehavior);
-
 close.addEventListener('click', closeModal);
