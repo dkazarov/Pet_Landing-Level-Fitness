fcb725ea1adb132234f580de1cdf2d70df7d1e84
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-01 03:48:46 +0300

    add fixed header

--- tasks/html.js
+++ tasks/html.js
@@ -1,20 +1,12 @@
 const { src, dest } = require("gulp");
 
-const webpHTML = require('gulp-webp-html');
+// const webpHTML = require('gulp-webp-html');
 const htmlmin = require("gulp-htmlmin");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
 const size = require("gulp-size");
 
 const html = () => {
    return src ('./src/*.html')
-     .pipe(plumber({
-       errorHandler: notify.onError(error => ({
-          title: 'HTML',
-          message: error.message
-       }))
-     }))
-     .pipe(webpHTML())
+    //  .pipe(webpHTML())
      .pipe(size({ title: "HTML Before compressing", }))
      .pipe( htmlmin({ collapseWhitespace: true, }) )
      .pipe(size({ title: "HTML After compressing", }))
